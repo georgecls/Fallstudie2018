3c0db4d18dc6b0b98b4d5083999bd0240c70fabc
Wulfman98 <43167606+Wulfman98@users.noreply.github.com>
2018-10-11 11:16:01 +0200

    Erster Versuch count
    Exception:"Can not issue SELECT via executeUpdate() or executeLargeUpdate()"

--- src/application/ControllerAuswertung.java
+++ src/application/ControllerAuswertung.java
@@ -1,5 +1,28 @@
 package application;
 
-public class ControllerAuswertung {
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ControllerAuswertung implements Initializable {
+	
+	@FXML private Label fieldTGesamt;
+	@FXML private Label fieldTFreigegeben;
+	@FXML private Label fieldTGenehmigt;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		try {
+			fieldTGesamt.setText(Integer.toString(Antrag.countAntraege()));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
