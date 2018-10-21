9f1bc87e6ce57111b656242c637403fc975f35ea
Wulfman98 <43167606+Wulfman98@users.noreply.github.com>
2018-10-21 12:58:12 +0200

    Update Icon

--- src/application/Main.java
+++ src/application/Main.java
@@ -35,7 +35,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.getIcons().add(new Image("/käpseleIcon.png"));
+		this.primaryStage.getIcons().add(new Image("/IconmitRing.png"));
 		loginWindow();
 	}
 	
